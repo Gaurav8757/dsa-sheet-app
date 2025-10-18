7110a601a7364ee143beaaf3fab4d6ab95753c52
Gaurav Kumar <mr.gauravkumar8757@gmail.com>
2025-10-18 16:58:41 +0530

    added  watch true

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,15 @@
 module.exports = {
   apps: [
-    // Backend
     {
       name: "backend",
       script: "npm",
       args: "start",
       cwd: "/data-sheet-app/backend",
-      watch: true, // optional
       env: {
-        NODE_ENV: "production",
         PORT: 5000,
-      },
-    },
-
-    // Frontend (Vite - Production build)
-    {
-      name: "frontend",
-      script: "npm",
-      args: "run preview", // matches your Vite script
-      cwd: "/data-sheet-app/frontend",
-      watch: true,
-      env: {
         NODE_ENV: "production",
-        PORT: 5173, // default Vite preview port
       },
     },
-
     {
       name: "frontend",
       script: "npx",
